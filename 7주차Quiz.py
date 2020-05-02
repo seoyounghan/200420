9b508b1556741ee4b4aa77caebae5f6d2a77677f
12yangyang25 <64002566+12yangyang25@users.noreply.github.com>
2020-05-02 18:25:05 +0900

    05/02 pm6:24

--- 7주차Quiz.py
+++ 7주차Quiz.py
@@ -2,6 +2,34 @@ from tkinter import *
 from tkinter.filedialog import *
 
 ## 함수 선언 부분 ##
+
+def brighter() :
+    global inImage, XSIZE, YSIZE
+    rgbString = ""
+    for i in range(0, XSIZE) :
+        tmpString = ""
+        for k in range(0, YSIZE) :
+            data = inImage[i][k] + 100
+            if data > 255:
+                data = 255
+            tmpString += "#%02x%02x%02x " % (data, data, data) # x 뒤에 한칸 공백, 16진수 문자열로 만듦
+        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
+    paper.put(rgbString)
+
+
+def darker() :
+    global inImage, XSIZE, YSIZE
+    rgbString = ""
+    for i in range(0, XSIZE) :
+        tmpString = ""
+        for k in range(0, YSIZE) :
+            data = inImage[i][k] - 100
+            if data < 0:
+                data = 0
+            tmpString += "#%02x%02x%02x " % (data, data, data) # x 뒤에 한칸 공백, 16진수 문자열로 만듦
+        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
+    paper.put(rgbString)
+
 def loadImage(fname) :
     global inImage, XSIZE, YSIZE
     fp = open(fname, 'rb')
@@ -53,9 +81,9 @@ effectmenu = Menu(mainmenu)
 mainmenu.add_cascade(label = "파일", menu = filemenu)
 mainmenu.add_cascade(label = "사진효과", menu = effectmenu)
 filemenu.add_command(label = "파일 열기", command = func_open)
-effectmenu.add_command(label = "밝게 하기")
+effectmenu.add_command(label = "밝게 하기", command = brighter)
 effectmenu.add_separator()
-effectmenu.add_command(label = "어둡게 하기")
+effectmenu.add_command(label = "어둡게 하기", command = darker)
 effectmenu.add_separator()
 effectmenu.add_command(label = "반전 이미지")
 effectmenu.add_separator()
