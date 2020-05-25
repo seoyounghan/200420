b8ed33900d8e6963e42f4cfc216920a3013e0ab6
paul <kimas3497@gmail.com>
2020-05-25 14:28:25 +0900

    0523 pm08:31 나머지 함수에 위치 좌표값을 추가하지 않아 추가함

--- kim1.py
+++ kim1.py
@@ -4,7 +4,7 @@ import turtle
 #변수 선언
 con, cur = None, None
 row = None
-i = 0
+
 batterinput = input() #사용자로 부터 입력받은 타자 이름 변수 -> 구체화 필요
 pitcherTypeinput = input() #사용자로부터 입력받은 투수종류 변수
 pitchTypeinput = input() #사용자로부터 입력받은 투구종류의 변수
@@ -36,7 +36,7 @@ def print_pitcherType() :
 
                 r, g, b = getRGB()
                 turtle.pencolor((r, g, b))
-            
+
                 t.shape("circle")
                 t.shapesize(1,1)
 
@@ -57,6 +57,9 @@ def print_pitchType() :
 
         batter = row[3]  #타자 이름을 저장
         pitchTpye = row[2]  #타구 종류를 저장
+
+        pointX = row[4]
+        pointY = row[5]
 
         if batter == batterinput:# 타자이름을 비교함
             if pitchTpye == pitcherTypeinput : #타구를 비교
@@ -85,6 +88,9 @@ def print_pitcherName() :
 
         batter = row[3]  #타자 이름을 저장
         pitchername = row[0]  #투수 이름을 저장
+
+        pointX = row[4]
+        pointY = row[5]
 
         if batter == batterinput: # 타자이름을 비교함
             if pitchername == pitcherNameinput : #투수 이름을 비교
