f104fcfcb6e5ed50fddf8dafd42b018254504dee
daeun <58421346+qor4@users.noreply.github.com>
2020-05-25 11:47:06 +0900

    처음창 (DB X)

--- python.py
+++ python.py
@@ -1,4 +1,6 @@
-from tkinter import*
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
 
 window = Tk()
 
@@ -13,28 +15,114 @@ window.config(menu=mainMenu)
 photo = PhotoImage(file="야구장사진.gif")
 label_image = Label(window, image=photo, bg="PINK")
 
+#변수설정
+pitcherName =""
+pitcherType =""
+pitchType =""
+batterName =""
+xPoint =""
+yPoint =""
+pitcherTypetext=""
+pitchTypetext=""
 
+#데이터베이스 설정
+con, cur = None, None
+sql = ""
+
+con = sqlite3.connect("userData")  # DB가 저장된 폴더까지 지정
+con = sqlite3.connect(r"C:\Users\Owner\Desktop\program\sqlite\userData")  # DB가 저장된 폴더까지 지정
+
+cur = con.cursor()
 
 #캔버스 설정
 canvas = Canvas(window, bd=2, width=560, height=400)
 canvas.create_image(280, 200, image = photo)
 canvas.pack(fill="both", side=LEFT)
 
-#마우스 클릭
+
+#그림 클릭
 def clickLeft(event) :
     print(event.x, event.y)
     canvas.create_oval(event.x - 3, event.y - 3, event.x + 3, event.y + 3, fill="red")
+    global xPoint
+    xPoint=event.x
+    global yPoint
+    yPoint=event.y
+
+#버튼 클릭
+def clickbutton4(): #투수 이름
+    global pitcherName
+    pitcherName=entry1.get()
+    print(entry1.get())
+
+def clickbutton5(): #좌투
+    global pitcherType
+    pitcherType=1
+    global pitcherTypetext
+    pitcherTypetext="좌투"
+    print(pitcherType)
+    print(pitcherTypetext)
+
+
+def clickbutton6(): #우투
+    global pitcherType
+    pitcherType = 2
+    global pitcherTypetext
+    pitcherTypetext="우투"
+    print(pitcherType)
+    print(pitcherTypetext)
+
+
+def clickbutton7(): #직구
+    global pitchType
+    pitchType=1
+    global pitchTypetext
+    pitchTypetext="직구"
+    print(pitchType)
+    print(pitchTypetext)
+
+def clickbutton8(): #슬라이더
+    global pitchType
+    pitchType=2
+    global pitchTypetext
+    pitchTypetext="슬라이더"
+    print(pitchType)
+    print(pitchTypetext)
+
+def clickbutton9(): #타자 이름
+    global batterName
+    batterName=entry2.get()
+    print(entry2.get())
+
+
+#맨 위 입력 버튼으로 데이터베이스에 데이터 저장
+
+def clickbutton1(): #입력 버튼
+    print("안녕")
+
+#    if ans==True:
+#        SaveLine()
+#        canvas.delete()
+
+def messageask():
+    messagebox.askquestion("확인", "투수 이름 : " + pitcherName  + "\n투수 정보 : " + pitcherTypetext + "\n구종 : " + pitchTypetext + "\n타자 이름 : " + batterName + "\n이 맞습니까?")
 
 
 #메뉴 설정
 fileMenu=Menu(mainMenu)
+helpMenu=Menu(mainMenu)
+
 mainMenu.add_cascade(label="파일", menu=fileMenu)
 fileMenu.add_command(label="입력")
 fileMenu.add_separator() #메뉴 구분선
 fileMenu.add_command(label="분석")
 
+mainMenu.add_cascade(label="Help", menu=helpMenu)
+helpMenu.add_command(label="도움말")
+
+
 #입력 정보
-button1=Button(window, text="입력", relief="groove", bg="skyblue")
+button1=Button(window, text="입력", relief="groove", bg="skyblue", command=messageask)
 button1.place(x=570, y=20)
 button2=Button(window, text="분석", relief="groove", bg="red")
 button2.place(x=610, y=20)
@@ -45,22 +133,22 @@ label1=Label(window, text="  투수 이름  ", relief="groove")
 label1.place(x=570, y=80)
 entry1=Entry(window, width=16) #투수정보
 entry1.place(x=570, y=103)
-button4=Button(window, text="입력", relief="groove", bg="gray")
+button4=Button(window, text="입력", relief="groove", bg="gray", command=clickbutton4)
 button4.place(x=690, y=100)
 
 label2=Label(window, text=" 투수 정보 선택 ", relief="groove")
 label2.place(x=570, y=150)
-button5=Button(window, text="   좌투   ", relief="groove", bg="gray")
+button5=Button(window, text="   좌투   ", relief="groove", bg="gray", command=clickbutton5)
 button5.place(x=570, y=175)
-button6=Button(window, text="   우투   ", relief="groove", bg="gray")
+button6=Button(window, text="   우투   ", relief="groove", bg="gray", command=clickbutton6)
 button6.place(x=640, y=175)
 
 
 label3=Label(window, text="  구종 선택  ", relief="groove")
 label3.place(x=570, y=230)
-button7=Button(window, text="   직구   ", relief="groove", bg="gray")
+button7=Button(window, text="   직구   ", relief="groove", bg="gray", command=clickbutton7)
 button7.place(x=570, y=255)
-button8=Button(window, text="슬라이더", relief="groove", bg="gray")
+button8=Button(window, text="슬라이더", relief="groove", bg="gray", command=clickbutton8)
 button8.place(x=640, y=255)
 
 
@@ -68,29 +156,20 @@ label4=Label(window, text="  타자 이름  ", relief="groove")
 label4.place(x=570, y=310)
 entry2=Entry(window, width=16) #타자정보
 entry2.place(x=570, y=333)
-button9=Button(window, text="입력", relief="groove", bg="gray")
+button9=Button(window, text="입력", relief="groove", bg="gray", command=clickbutton9)
 button9.place(x=690, y=330)
 
+#데이터베이스 설정
+def SaveLine():  ##데이터베이스에 데이터 저장
 
+    sql = "INSERT INTO userData VALUES(" + pitcherName + "," + pitcherType + "," + pitchType + "," + batterName + "," + xPoint + "," + yPoint + ");"
 
-i=1 #list 순서
-ls=Listbox(window, width=140, height=400, relief="sunken") #리스트 크기 지정
-ls.insert(1, "ㅇㅇㅇ") #리스트 뜨는지 안뜨는지
+    cur.execute(sql)
 
-#list 추가, 삭제 함수
-def insert(number, name, i):
-    ls.insert(i, name)
-    i=i+1
-def delete(i):
-    ls.delete(ANCHOR) #ANCHOR 첫번째가 지워짐
-    i=i-1
+    con.commit()
+    con.close()
 
 
 canvas.bind("<Button-1>", clickLeft)
-
-
-#label_image.pack(side=LEFT)
-#ls.pack(side=RIGHT)
-
 
 window.mainloop()
